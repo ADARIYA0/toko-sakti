82a2dcdd3d415e4967a0791546c4e82e84a39412
ADARIYA0 <adrianianimportant@gmail.com>
2024-12-17 01:33:43 +0700

    Product list and pagination

--- Modules/Shop/Entities/Product.php
+++ Modules/Shop/Entities/Product.php
@@ -55,32 +55,44 @@ class Product extends Model
     {
         return \Modules\Shop\Database\factories\ProductFactory::new();
     }
+
     public function user()
     {
         return $this->belongsTo('App\Models\User');
     }
+
     public function inventory()
     {
         return $this->hasOne('Modules\Shop\Entities\ProductInventory');
     }
+
     public function variants()
 	{
 		return $this->hasMany('Modules\Shop\Entities\Product', 'parent_id')->orderBy('price', 'ASC');
 	}
+
     public function categories()
     {
         return $this->belongsToMany('Modules\Shop\Entities\Category', 'shop_categories_products', 'product_id', 'category_id'); //phpcs:ignore
     }
+
     public function tags()
     {
         return $this->belongsToMany('Modules\Shop\Entities\Tag', 'shop_products_tags', 'product_id', 'tag_id');
     }
+
     public function attributes()
 	{
 		return $this->hasMany(ProductAttribute::class, 'product_id');
 	}
+
     public function images()
 	{
 		return $this->hasMany('Modules\Shop\Entities\ProductImage', 'product_id');
 	}
+    
+    public function getPriceLabelAttribute()
+    {
+        return number_format($this->price);
+    }
 }
