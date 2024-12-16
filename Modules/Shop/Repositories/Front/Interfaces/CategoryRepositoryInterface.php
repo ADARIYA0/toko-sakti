81f5b1dbeb5f213c70e5e6618833705abde3f77f
ADARIYA0 <adrianianimportant@gmail.com>
2024-12-17 02:12:01 +0700

    Filter products by category

---
+++ Modules/Shop/Repositories/Front/Interfaces/CategoryRepositoryInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Modules\Shop\Repositories\Front\Interfaces;
+
+interface CategoryRepositoryInterface
+{
+    public function findAll($options = []);
+    public function findBySlug($slug);
+}
+
+?>
