637208f0595bd3c5f7e22b787548f38d45c7c73a
ADARIYA0 <adrianianimportant@gmail.com>
2024-12-17 18:30:43 +0700

    Sorting products by price and date

--- Modules/Shop/Repositories/Front/ProductRepository.php
+++ Modules/Shop/Repositories/Front/ProductRepository.php
@@ -15,6 +15,8 @@ class ProductRepository implements ProductRepositoryInterface
         $perPage = $options['per_page'] ?? null;
         $categorySlug = $options['filter']['category'] ?? null;
         $tagSlug = $options['filter']['tag'] ?? null;
+        $priceFilter = $options['filter']['price'] ?? null;
+        $sort = $options['sort'] ?? null;
 
         $products = Product::with(['categories', 'tags']);
 
@@ -36,6 +38,15 @@ class ProductRepository implements ProductRepositoryInterface
             $products = $products->whereHas('tags', function ($query) use ($tag) {
                 $query->where('shop_tags.id', $tag->id);
             });
+        }
+
+        if ($priceFilter) {
+            $products = $products->where('price', '>=', $priceFilter['min'])
+                ->where('price', '<=', $priceFilter['max']);
+        }
+
+        if ($sort) {
+            $products = $products->orderBy($sort['sort'], $sort['order']);
         }
 
         if ($perPage) {
